d77775a5f1fa08ae469cf25a4225f7e4c43f506c
desims <sdesimayasari@gmail.com>
2024-11-30 11:59:49 +0700

    Your commit message

--- receive.py
+++ receive.py
@@ -1,1 +1,30 @@
 
+import pika, sys, os
+
+def main():
+    # Establish a connection with the RabbitMQ server
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    # Redeclare the queue because we are unsure whether or not the queue exists. 
+    channel.queue_declare(queue='hello')
+
+    # RabbitMQ will trigger this function once we receive a message.
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {body}")
+
+    # Subscribe to the "hello" queue. 
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
--- send.py
+++ send.py
@@ -1,1 +1,15 @@
 
+import pika
+
+# Create a connection with localhost
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+# Create a queue name "hello"
+channel.queue_declare(queue='hello')
+
+# Send a message to a default exchange
+# routing_key = queue name. 
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
